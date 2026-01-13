d468787a609cffcd075dba02bdc586920eae3ef0
E0min <bbok4yo@gmail.com>
2026-01-13 16:33:37 +0900

    chore:

--- front/src/app/register/page.tsx
+++ front/src/app/register/page.tsx
@@ -13,7 +13,7 @@ export default function RegisterPage() {
     <div className="flex flex-col min-h-screen bg-white text-gray-900">
       <header className="w-full p-4 bg-white text-gray-900 flex items-center justify-between  border-gray-200">
         <Link href="/" className="text-2xl font-bold">
-          ChatGraph
+          Chat-Graph
         </Link>
       </header>
       <main className="flex items-center justify-center">
